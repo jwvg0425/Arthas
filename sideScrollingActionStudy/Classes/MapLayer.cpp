17812c92bf4ab4ee3b0177ad099c704fe295d166
ozt88 <kl1467@gmail.com>
2014-10-05 19:50:06 +0900

    mapLayer

--- sideScrollingActionStudy/Classes/MapLayer.cpp
+++ sideScrollingActionStudy/Classes/MapLayer.cpp
@@ -9,7 +9,6 @@ bool MapLayer::init()
 	{
 		return false;
 	}
-	m_Gravity = -98.f;
 	initMap( "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n"
 			 "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n"
 			 "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n"
@@ -27,9 +26,9 @@ bool MapLayer::init()
 			 "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n"
 			 "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n"
 			 "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n"
-			 "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n"
-			 "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n"
-			 "1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1\n" );
+			 "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n"
+			 "0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0\n"
+			 "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0\n" );
 
 	m_Player = Player::create();
 	m_Player->setAnchorPoint(Point(0.5, 0.5));
@@ -44,38 +43,40 @@ bool MapLayer::init()
 
 void MapLayer::update(float dTime)
 {
-	m_Player->setOuterForce( calculateOuterForce( m_Player->getVelcotiy() , m_Player->getRect(), dTime ));
-
-	Point pos = m_Player->getPosition();
-
-	Point mapPos;
-
-	mapPos.x = 320 - pos.x;
-	mapPos.y = 240 - pos.y;
-
-	if (mapPos.x > 0)
-	{
-		mapPos.x = 0;
-	}
-
-	if (mapPos.y > 0)
-	{
-		mapPos.y = 0;
-	}
-
-	if (mapPos.x < -m_BoxWidthNum * 32 + 640)
-	{
-		mapPos.x = -m_BoxWidthNum * 32 + 640;
-	}
-
-	if (mapPos.y < -m_BoxHeightNum * 32 + 480)
-	{
-		mapPos.y = -m_BoxHeightNum * 32 + 480;
-	}
-
-
-	this->setPosition(mapPos);
+	setViewPosition();
 }
+
+void MapLayer::setViewPosition()
+{
+	Point pos = m_Player->getPosition();
+	Point mapPos;
+	Size winSize = Director::getInstance()->getWinSize();
+	mapPos.x = winSize.width*this->getAnchorPoint().x - pos.x;
+	mapPos.y = winSize.height*this->getAnchorPoint().y - pos.y;
+
+	if( mapPos.x > -m_BoxSize.width )
+	{
+		mapPos.x = -m_BoxSize.width;
+	}
+
+	if( mapPos.y > -m_BoxSize.height )
+	{
+		mapPos.y = -m_BoxSize.height;
+	}
+
+	if( mapPos.x < -m_BoxWidthNum * ( m_BoxSize.width - 1 ) + winSize.width )
+	{
+		mapPos.x = -m_BoxWidthNum * ( m_BoxSize.width - 1 ) + winSize.width;
+	}
+
+	if( mapPos.y < -m_BoxHeightNum *  ( m_BoxSize.height - 1 ) + winSize.height )
+	{
+		mapPos.y = -m_BoxHeightNum *  ( m_BoxSize.height - 1 ) + winSize.height;
+	}
+
+	this->setPosition( mapPos );
+}
+
 
 void MapLayer::initMap(char* mapData)
 {
@@ -87,58 +88,51 @@ void MapLayer::initMap(char* mapData)
 	m_MapRawData = UserDefault::getInstance()->getStringForKey( "mapData" );
 
 	char* rawValue;
-	char rawData[8192];
+	char rawData[MAX_DATA_SIZE];
 
 	strcpy( rawData , m_MapRawData.c_str() );
 
 	rawValue = strtok( rawData , " \n" );
-
-	for( int y = m_BoxHeightNum + 1; y >= 0; y-- )
+	int value;
+	for( int y = m_BoxHeightNum - 1; y >= 0; y-- )
 	{
-		for( int x = 0; x <= m_BoxWidthNum + 1; x++ )
+		for( int x = 0; x < m_BoxWidthNum; x++ )
 		{
-			if( y == 0 || x == 0 || y == m_BoxHeightNum + 1 || x == m_BoxWidthNum + 1 )
-			{
-				m_MapData[m_BoxWidthNum*y + x]=  0;
-				continue;
-			}
-
-			int value = atoi( rawValue );
-
+			value = atoi( rawValue );
 			m_MapData[m_BoxWidthNum*y + x] = value;
 
 			rawValue = strtok( nullptr , " \n" );
 		}
 	}
 
-	for( int y = 1; y <= m_BoxHeightNum; y++ )
+	for( int y = 0; y < m_BoxHeightNum; y++ )
 	{
-		for( int x = 1; x <= m_BoxWidthNum + 1; x++ )
+		for( int x = 0; x < m_BoxWidthNum; x++ )
 		{
 			Sprite* sprite = nullptr;
-			switch( m_MapData[y*( m_BoxWidthNum + 2 ) + x] )
+			switch( m_MapData[y*m_BoxWidthNum + x] )
 			{
 				case 0:
-					if( m_MapData[( y - 1 )*( m_BoxWidthNum + 2 ) + x - 1] == 1 )
+					if( m_MapData[( y - 1 )*( m_BoxWidthNum ) + x - 1] == 1 ) 
 					{
-						if( m_MapData[( y - 1 )*( m_BoxWidthNum + 2 ) + x] == 1 )
+						if( m_MapData[( y - 1 )*( m_BoxWidthNum  ) + x] == 1 )
 						{
-							if( m_MapData[y*( m_BoxWidthNum + 2 ) + x - 1] == 1 )
+							if( m_MapData[y*( m_BoxWidthNum ) + x - 1] == 1 ) //(x, y) 박스 밑에 1 박스 왼쪽 아래 1, 박스 왼쪽 1
 							{
 								sprite = Sprite::createWithSpriteFrameName( "tile_side_lowercorner.png" );
 							}
-							else
+							else //(x, y) 박스 밑에 1 박스 왼쪽 아래 1, 박스 왼쪽 0
 							{
 								sprite = Sprite::createWithSpriteFrameName( "tile_back.png" );
 							}
 						}
-						else
+						else 
 						{
-							if( m_MapData[y*( m_BoxWidthNum + 2 ) + x - 1] == 1 )
+							if( m_MapData[y*( m_BoxWidthNum ) + x - 1] == 1 )  //(x, y) 박스 밑에 0 박스 왼쪽 아래 1, 박스 왼쪽 1
 							{
 								sprite = Sprite::createWithSpriteFrameName( "tile_side.png" );
 							}
-							else
+							else //(x, y) 박스 밑에 0 박스 왼쪽 아래 1, 박스 왼쪽 0	
 							{
 								sprite = Sprite::createWithSpriteFrameName( "tile_side_uppercorner.png" );
 							}
@@ -146,11 +140,11 @@ void MapLayer::initMap(char* mapData)
 					}
 					else
 					{
-						if( m_MapData[( y - 1 )*( m_BoxWidthNum + 2 ) + x] == 1 )
+						if( m_MapData[( y - 1 )*( m_BoxWidthNum) + x] == 1 ) //(x, y) 박스 밑에 1 박스 왼쪽 아래 0,
 						{
 							sprite = Sprite::createWithSpriteFrameName( "tile_back_corner.png" );
 						}
-						else if( m_MapData[y*( m_BoxWidthNum + 2 ) + x - 1] == 1 )
+						else if( m_MapData[y*( m_BoxWidthNum ) + x - 1] == 1 ) //(x, y) 박스 왼쪽 1 박스 왼쪽 아래 0,
 						{
 							sprite = Sprite::createWithSpriteFrameName( "tile_side_corner.png" );
 						}
@@ -163,61 +157,9 @@ void MapLayer::initMap(char* mapData)
 			if( sprite != nullptr )
 			{
 				sprite->setAnchorPoint( Point::ZERO );
-				sprite->setPosition( Point( ( x - 1 ) * 32 , (y - 1)  * 32 ) );
+				sprite->setPosition( Point( x * 32 , y * 32 ) );
 				this->addChild( sprite );
 			}
 		}
 	}
-}
-
-bool MapLayer::checkAvailablePosition( cocos2d::Point position )
-{
-	int boxWidthIdx = position.x / m_BoxSize.width;
-	int boxHeightIdx = position.y / m_BoxSize.height;
-	int checkIdx = m_BoxWidthNum*boxHeightIdx + boxWidthIdx;
-	if( checkIdx >= m_MapData.size() )
-	{
-		return false;
-	}
-	bool result = !m_MapData[checkIdx];
-	return result;
-}
-
-bool MapLayer::checkAvailableRect( cocos2d::Rect rect )
-{
-	Point checkPos = rect.origin;
-	for( int anchorX = 0; anchorX < 2; ++anchorX )
-	{
-		for( int anchorY = 0; anchorY < 2; ++anchorY )
-		{
-			checkPos.x += rect.size.width*anchorX;
-			checkPos.y += rect.size.height*anchorY;
-			if( !checkAvailablePosition( checkPos ) )
-			{
-				return false;
-			}
-		}
-	}
-	return true;
-}
-
-cocos2d::Vec2 MapLayer::calculateOuterForce( cocos2d::Vec2 objectVelocity , cocos2d::Rect objectRect , float dTime )
-{
-	Vec2 resultVector = Vec2( 0 , 0 );
-	Rect nextRect = Rect(objectRect);
-	nextRect.origin.x += objectVelocity.x*dTime;
-	if( !checkAvailableRect( nextRect ) )
-	{
-		resultVector.x = -objectVelocity.x;
-	}
-	nextRect.origin.y += 10 + objectVelocity.y*dTime;
-	if( !checkAvailableRect( nextRect ) )
-	{
-		resultVector.y = -objectVelocity.y;
-	}
-	else
-	{
-		resultVector.y = m_Gravity*dTime;
-	}
-	return resultVector;
 }
--- sideScrollingActionStudy/Classes/MapLayer.h
+++ sideScrollingActionStudy/Classes/MapLayer.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include "cocos2d.h"
+#define MAX_DATA_SIZE 1024*10
 
 class Player;
 
@@ -10,16 +11,13 @@ public:
 	virtual bool	init();
 	void			update(float dTime);
 	void			initMap(char* mapData);
-	bool			checkAvailablePosition(cocos2d::Point position);
-	bool			checkAvailableRect( cocos2d::Rect rect );
-	cocos2d::Vec2	calculateOuterForce( cocos2d::Vec2 objectVelocity , cocos2d::Rect objectRect, float dTime);
+	void			setViewPosition();
 
 	CREATE_FUNC( MapLayer );
 
 private:
 	cocos2d::Rect		m_MapRect;
 	cocos2d::Size		m_BoxSize;
-	float				m_Gravity;
 	int					m_BoxWidthNum, m_BoxHeightNum;
 	std::string			m_MapRawData;
 	std::map<int, int>	m_MapData;
