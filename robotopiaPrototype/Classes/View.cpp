0f77e4aa9402a3d9aa0e83e1e2409bb8649e6234
abiles <pgalpga@hanmail.net>
2014-10-06 22:25:09 +0900

    LastPoint dont Move

--- robotopiaPrototype/Classes/View.cpp
+++ robotopiaPrototype/Classes/View.cpp
@@ -4,42 +4,39 @@
 USING_NS_CC;
 
 
-void View::setViewPort( GameLayer* scene , cocos2d::Point standardPoint , cocos2d::Point anchorPoint )
+void View::setViewPort( GameLayer* scene , Point standardPoint , Point anchorPoint )
 {
 	Rect mapRect = scene->getMapRect();
-	float anchorX = Director::getInstance()->getWinSize().width * anchorPoint.x;
-	float anchorY = Director::getInstance()->getWinSize().height * anchorPoint.y;
-
-
-
+	float windowWidth = Director::getInstance()->getWinSize().width;
+	float windowHeight = Director::getInstance()->getWinSize().height;
+	float anchorX = windowWidth * anchorPoint.x;
+	float anchorY = windowHeight * anchorPoint.y;
+	
 	float moveX = anchorX - standardPoint.x;
 	float moveY = anchorY - standardPoint.y;
 
 
+	//이걸하지 말라고 해야돼. 
+	// 언제? 이동하면 화면에 짤릴 때 
+	if (standardPoint.x + anchorPoint.x * windowWidth > mapRect.size.width ||
+		standardPoint.x - anchorPoint.x * windowWidth < 0||
+		standardPoint.y + anchorPoint.y * windowHeight > mapRect.size.height||
+		standardPoint.y + anchorPoint.y * windowHeight > mapRect.size.height)
+	{
+		return;
+	}
+	else
+	{
+		scene->setPosition(moveX, moveY);
 
-	scene->setPosition(moveX, moveY);
+	}
+
+	
 }
 
 
 
-void View::setViewPort( GameLayer* scene , cocos2d::Rect rect , Point anchorPoint )
-{
-	float nodeWidth = scene->getContentSize().width;
-	float nodeHeight = scene->getContentSize().height;
-	
-	float centerX = nodeWidth / 2;
-	float centerY = nodeHeight / 2;
-	
-	float moveX = centerX - rect.origin.x;
-	float moveY = centerY - rect.origin.y;
-
-	scene->setPosition(moveX,moveY);
-
-	return;
-
-}
-
-void View::setViewPortWithHighlight()
+void View::setViewPortWithHighlight(GameLayer* inputScene, cocos2d::Rect stadardRect)
 {
 	return;
 }
