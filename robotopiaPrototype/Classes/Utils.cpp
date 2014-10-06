26c516c669a1a27975fb19bb8bf59e4433bb93f5
jwvg0425 <jwvg0425@naver.com>
2014-10-06 17:25:15 +0900

    keyboard
    keyboard

---
+++ robotopiaPrototype/Classes/Utils.cpp
@@ -0,0 +1,79 @@
+﻿#include "Utils.h"
+
+USING_NS_CC;
+
+std::map<cocos2d::EventKeyboard::KeyCode, KeyState> KeyStateManager::m_KeyStates;
+std::map<cocos2d::EventKeyboard::KeyCode, KeyState> KeyStateManager::m_PrevKeyStates;
+std::map<cocos2d::EventKeyboard::KeyCode, KeyState> KeyStateManager::m_FinalKeyStates;
+
+KeyState KeyStateManager::getKeyboardState(cocos2d::EventKeyboard::KeyCode keyCode)
+{
+	return m_FinalKeyStates[keyCode];
+}
+
+void KeyStateManager::receiveKeyboardData(cocos2d::Layer* layer)
+{
+	auto sentinel = KeyStateSentinel::create();
+
+	layer->addChild(sentinel);
+}
+
+bool KeyStateSentinel::init()
+{
+	if (!Node::init())
+	{
+		return false;
+	}
+
+	auto keyListener = EventListenerKeyboard::create();
+	keyListener->onKeyPressed = CC_CALLBACK_2(KeyStateSentinel::onKeyPressed, this);
+	keyListener->onKeyReleased = CC_CALLBACK_2(KeyStateSentinel::onKeyReleased, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(keyListener, this);
+
+
+	for (EventKeyboard::KeyCode i = EventKeyboard::KeyCode::KEY_NONE; i < EventKeyboard::KeyCode::KEY_PLAY; i = (EventKeyboard::KeyCode)((int)i + 1))
+	{
+		KeyStateManager::m_PrevKeyStates[i] = NONE;
+		KeyStateManager::m_KeyStates[i] = NONE;
+	}
+
+	return true;
+}
+
+void KeyStateSentinel::update(float dTime)
+{
+	for (auto state : KeyStateManager::m_KeyStates)
+	{
+		KeyState prevState = KeyStateManager::m_PrevKeyStates[state.first];
+		KeyState nowState = KeyStateManager::m_KeyStates[state.first];
+
+		if (prevState == NONE && nowState == NONE)
+		{
+			KeyStateManager::m_FinalKeyStates[state.first] = NONE;
+		}
+		else if (prevState == NONE && nowState == PRESS)
+		{
+			KeyStateManager::m_FinalKeyStates[state.first] = PRESS;
+		}
+		else if (prevState == PRESS && nowState == NONE)
+		{
+			KeyStateManager::m_FinalKeyStates[state.first] = RELEASE;
+		}
+		else if (prevState == PRESS && nowState == PRESS)
+		{
+			KeyStateManager::m_FinalKeyStates[state.first] = HOLD;
+		}
+
+		KeyStateManager::m_PrevKeyStates[state.first] = state.second;
+	}
+}
+
+void KeyStateSentinel::onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event)
+{
+	KeyStateManager::m_KeyStates[keyCode] = PRESS;
+}
+
+void KeyStateSentinel::onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event)
+{
+	KeyStateManager::m_KeyStates[keyCode] = NONE;
+}
--- robotopiaPrototype/Classes/Utils.h
+++ robotopiaPrototype/Classes/Utils.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include "cocos2d.h"
 
 #define ABSTRACT virtual
 #define OVERRIDE virtual
@@ -16,4 +17,42 @@ enum Direction
 	DIR_UP = 2,
 	DIR_RIGHT = 4,
 	DIR_DOWN = 8,
+};
+
+enum KeyState
+{
+	NONE, // 뗀 상태
+	PRESS, //딱 누름
+	HOLD, //꾹 누르는 중
+	RELEASE, // 딱 뗌
+};
+
+class KeyStateManager
+{
+	friend class KeyStateSentinel;
+
+public:
+	KeyState getKeyboardState(cocos2d::EventKeyboard::KeyCode keyCode);
+	static void receiveKeyboardData(cocos2d::Layer* layer); //layer에서 호출하면 이제 그 레이어에서는 키보드 정보가 자동으로 업데이트됨.
+private:
+	static std::map<cocos2d::EventKeyboard::KeyCode, KeyState> m_KeyStates;
+	static std::map<cocos2d::EventKeyboard::KeyCode, KeyState> m_PrevKeyStates;
+	static std::map<cocos2d::EventKeyboard::KeyCode, KeyState> m_FinalKeyStates;
+};
+
+//키보드 누르는 정보를 송신해줌.
+class KeyStateSentinel : public cocos2d::Node
+{
+	friend class KeyStateManager;
+
+public:
+	OVERRIDE bool	init();
+
+	void			onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+	void			onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+
+	void			update(float dTime);
+
+	CREATE_FUNC(KeyStateSentinel);
+private:
 };
