ef98366fa66859cc86e761f956456c9dcf52a2bd
abiles <pgalpga@hanmail.net>
2014-10-06 22:28:25 +0900

    LPDM2

--- robotopiaPrototype/Classes/View.h
+++ robotopiaPrototype/Classes/View.h
@@ -10,11 +10,8 @@ public:
 
 	// standardPoint로 기준점이 되는 점을 넘겨라, anchorPoint(0~1)
 	static void setViewPort( GameLayer* scene , cocos2d::Point standardPoint , cocos2d::Point anchorPoint );
-	//
-	static void setViewPort( GameLayer* inputScene , cocos2d::Rect stadardRect , cocos2d::Point anchorPoint );
-
-	//몇 배로 크게 해줘라 
-	static void setViewPortWithHighlight();
+	
+	static void setViewPortWithHighlight(GameLayer* inputScene, cocos2d::Rect stadardRect);
 
 	
 private:
