2bd4bb64ee1d90c9940a1a3ac28897dabd1bd438
Jaewoo Woo <gewoose@gmail.com>
2014-10-07 17:39:27 +0900

    UIedit

--- robotopiaPrototype/Classes/UILayer.cpp
+++ robotopiaPrototype/Classes/UILayer.cpp
@@ -6,35 +6,30 @@ USING_NS_CC;
 
 bool UILayer::init()
 {
-	auto gameLayer = ((WorldScene*)this->getParent())->getGameLayer();
-
-	cocos2d::Rect winSize = gameLayer->getWinRect();
-	int winHeight = winSize.size.height;
+	Size winSize = Director::getInstance()->getWinSize();
+	int winHeight = winSize.height;
 	
 	auto sprHPContainer = Sprite::create("Container_HP.png");
-	sprHPContainer->setZOrder(10);
 	Point pHP;
 	pHP.x = 20;
-	pHP.y = 20;
+	pHP.y = winHeight - 20;
 	sprHPContainer->setPosition(pHP);
-	this->addChild(sprHPContainer);
+	this->addChild(sprHPContainer, 10);
 	
 	auto sprSTEContainer = Sprite::create("Container_STE.png");
-	sprSTEContainer->setZOrder(10);
 	Point pSTE;
 	pSTE.x = 20;
-	pSTE.y = 80;
+	pSTE.y = winHeight - 80;
 	sprSTEContainer->setPosition(pSTE);
-	this->addChild(sprSTEContainer);
+	this->addChild(sprSTEContainer, 10);
 
-	m_Player = gameLayer->getPlayer();
-	
 	this->scheduleUpdate();
 	return true;
 }
 
 void UILayer::update(float dTime)
 {
-
+	auto gameLayer = ((WorldScene*)this->getParent())->getGameLayer();
+	m_Player = gameLayer->getPlayer();
 }
 
